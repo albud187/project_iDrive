1275b14925f398f95c303f99bdbd76d69ce6158e
Alfa Budiman <63726302+albud187@users.noreply.github.com>
2020-09-17 20:33:38 -0300

    user auth templates

--- project_iDrive/settings.py
+++ project_iDrive/settings.py
@@ -53,6 +53,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'bootstrap4',
+    'app_accounts_auth',
 ]
 
 MIDDLEWARE = [
