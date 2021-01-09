22df4e5d68fb51047368ed678eed83bb563073aa
albud187 <alfa.budiman@outlook.com>
2021-01-09 11:25:09 -0400

    update

--- vehicle_analytics/views.py
+++ vehicle_analytics/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
-
+import pickle
 # Create your views here.
+with open('vehicle_price_model.pickle', 'rb') as handle:
+    vehicle_price_model = pickle.load(handle)
+
+
+def price_suggestion(price_model, makemodel,year,milage):
+    coefficients = price_model[makemodel]
+    return((year-2000)*coefficients[0] + milage*coefficients[1]+coefficients[2])
